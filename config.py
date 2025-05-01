960d6769557c977d5d9ec5a9da432fc18d2b76a7
Dden59 <prmden90@gmail.com>
2025-05-01 23:25:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7814219690:AAGCRIpcPsrQugpn4WjCoKwGcpAWP8f3mO4"
 CHANNEL_ID = -1002674530242  # ID канала с постбэками
-PARTNER_LINK = "https://1wsewz.com/?sub1="
+PARTNER_LINK = "https://1wwcr.com/?sub1="
 WEBAPP_LINK = "https://dden59.github.io/keeper/"
