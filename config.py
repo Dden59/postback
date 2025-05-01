313f7907280081f1cae9f12c11c06a9331bc8bb9
Dden59 <prmden90@gmail.com>
2025-05-01 18:09:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "7814219690:AAGCRIpcPsrQugpn4WjCoKwGcpAWP8f3mO4"
+CHANNEL_ID = -1002255720733  # ID канала с постбэками
+PARTNER_LINK = "https://1wsewz.com/?sub1="
+WEBAPP_LINK = "https://dden59.github.io/keeper/"
