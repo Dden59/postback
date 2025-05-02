a454190a9335ca21f6864ce8942e85ba3b871fad
Dden59 <prmden90@gmail.com>
2025-05-02 20:03:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 # Все значения получаются из переменных окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Берется из Railway
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1002665040288))  # Значение по умолчанию уже новое!
-PARTNER_LINK = os.getenv("PARTNER_LINK", "https://1wsewz.com/?sub1=")
+PARTNER_LINK = os.getenv("PARTNER_LINK", "https://1wwcr.com/?sub1=")
 WEBAPP_LINK = os.getenv("WEBAPP_LINK", "https://dden59.github.io/keeper/")
