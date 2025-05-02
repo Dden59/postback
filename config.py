eb15f779f2dda0dcdf3c6e85852c427976e5ba3a
Dden59 <prmden90@gmail.com>
2025-05-02 19:55:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-BOT_TOKEN = "7814219690:AAGCRIpcPsrQugpn4WjCoKwGcpAWP8f3mO4"
-CHANNEL_ID = -1002674530242  # ID канала с постбэками
-PARTNER_LINK = "https://1wwcr.com/?sub1="
-WEBAPP_LINK = "https://dden59.github.io/keeper/"
+import os
+
+# Все значения получаются из переменных окружения
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Берется из Railway
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1002665040288))  # Значение по умолчанию уже новое!
+PARTNER_LINK = os.getenv("PARTNER_LINK", "https://1wsewz.com/?sub1=")
+WEBAPP_LINK = os.getenv("WEBAPP_LINK", "https://dden59.github.io/keeper/")
