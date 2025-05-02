6d0d0bd8300235cde0d87781f981b9700826a494
Dden59 <prmden90@gmail.com>
2025-05-03 00:09:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
-# Все значения получаются из переменных окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Берется из Railway
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1002665040288))  # Значение по умолчанию уже новое!
-PARTNER_LINK = os.getenv("PARTNER_LINK", "https://1wwcr.com/?sub1=")
+# Обязательные параметры
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1002665040288))  # Ваш канал для постбеков
+PARTNER_LINK = os.getenv("PARTNER_LINK", "https://1wwcr.com/").rstrip('/') + '/'
 WEBAPP_LINK = os.getenv("WEBAPP_LINK", "https://dden59.github.io/keeper/")
+
+# Жёсткие проверки
+assert len(BOT_TOKEN) == 46, "Неверная длина токена! Проверьте BOT_TOKEN"
+assert CHANNEL_ID < 0, "CHANNEL_ID должен быть отрицательным (канал/супергруппа)"
+assert WEBAPP_LINK.startswith('https://'), "WebApp требует HTTPS!"
